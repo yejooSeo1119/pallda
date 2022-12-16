d7cea3cf0d9b6571e32a814b16a3caaf041664e4
yejooSeo1119 <108320583+yejooSeo1119@users.noreply.github.com>
2022-12-16 17:59:39 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -50,3 +50,18 @@ $(document).ready(function () {
 });
 
 
+//find tab
+$(document).ready(function () {
+
+    $('.tab ul li').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('.tab ul li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    });
+
+});
+
